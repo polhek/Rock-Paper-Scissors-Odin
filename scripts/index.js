ce3c7d9491d616da103ed95a462ae4cb8fde143d
polhek <ziga.groselj@gmail.com>
2020-06-27 16:35:56 +0200

    Finished all the tasks, will probably come back to it, and code it in a better way, like limiting the score to 5 in a better way

--- scripts/index.js
+++ scripts/index.js
@@ -7,11 +7,14 @@ const game = () => {
     const scissors = document.getElementById("scissors");
     const userScore = document.getElementById("user_score");
     const computerScores = document.getElementById("computer_score");
+    const winningText = document.querySelector(".winning_text p");
+
+    function reloadThePage() {
+        window.location.reload();
+    };
     
 
-    
-    
-    
+
     function computerPlay () {
         // array of computer options
         const computerOptions = ["rock", "paper", "scissors"];
@@ -25,11 +28,10 @@ const game = () => {
     
     function singleRound(playerSelection, computerSelection) {
     
-   
-
         // Tie
         if (playerSelection === computerSelection) {
             console.log("It is a tie.");
+            winningText.textContent = "It is a tie.";
             return;
         } 
     
@@ -37,9 +39,11 @@ const game = () => {
         if (playerSelection === "rock") {
             if (computerSelection === "scissors") {
                 console.log("You won the round.");
+                winningText.textContent = "You won the round.";
                 playerScore++;
                 return;
             } else {
+                winningText.textContent = "You lost the round.";
                 console.log("You lost the round.");
                 computerScore++;
                 return;
@@ -49,11 +53,13 @@ const game = () => {
         // Paper
         if (playerSelection === "paper") {
             if (computerSelection === "scissors") {
+                winningText.textContent = "You lost the round.";
                 console.log("You lost the round.");
                 computerScore++;
                 return;
             } else {
                 console.log("You won the round.");
+                winningText.textContent = "You won the round.";
                 playerScore++;
                 return;
             }
@@ -63,10 +69,12 @@ const game = () => {
         if (playerSelection === "scissors") {
             if (computerSelection === "rock") {
                 console.log("You lost the round.");
+                winningText.textContent = "You lost the round.";
                 computerScore++;
                 return;
             } else {
                 console.log("You won the round.");
+                winningText.textContent = "You won the round.";
                 playerScore++;
                 return;
             }
@@ -77,40 +85,73 @@ const game = () => {
     };
 
     const playerChoice = () => {
-        // player choose rock
-        rock.addEventListener('click', function() {
-            //console.log("rock");
-            game('rock')
-        })
-        // player choose paper
-        paper.addEventListener('click', function() {
-            //console.log("paper");
-            game('paper');
-        })
+      rock.addEventListener("click", function () {
+        //console.log("rock");
+        game("rock");
+      });
+      // player choose paper
+      paper.addEventListener("click", function () {
+        //console.log("paper");
+        game("paper");
+      });
 
-        // player choose scissors
-        scissors.addEventListener('click', function() {
-            //console.log('scissors');
-            game('scissors');
-        })
+      // player choose scissors
+      scissors.addEventListener("click", function () {
+        //console.log('scissors');
+        game("scissors");
+      });
 
-    function game(playerSelection) {
-       
+      const startTheGame = () => {
+        const intro = document.querySelector(".play_game");
+        const match = document.querySelector(".choices");
+        const playButton = document.querySelector(".play_game_button");
+
+        playButton.addEventListener("click", () => {
+          intro.classList.add("fadeOut");
+          match.classList.add("fadeIn");
+          match.style.removeProperty("display");
+        });
+      };
+
+      function game(playerSelection) {
         const computerSelection = computerPlay();
         singleRound(playerSelection, computerSelection);
-        
+
         userScore.textContent = playerScore;
         computerScores.textContent = computerScore;
-
         // Play the game to 5
-        
-    };
-    
-    
+        if (playerScore === 5 && computerScore < 5) {
+          winningText.textContent =
+            "You have won with the score of " +
+            playerScore +
+            " to " +
+            computerScore +
+            "...";
+
+          return;
+        } else if (playerScore < 5 && computerScore === 5) {
+          winningText.textContent =
+            "Computer has won with the score of " +
+            computerScore +
+            " to " +
+            playerScore +
+            "...";
+
+          return;
+        };
+
+        function reloadThePage() {
+            window.location.reload();
+        };
+
+        if (playerScore > 5 || computerScore > 5) {
+            setTimeout(reloadThePage(), 115000);
+        };
 
         
+      }
+      startTheGame();
     };
-
     playerChoice();
     
     
